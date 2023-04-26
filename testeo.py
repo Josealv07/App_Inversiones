0a92b4efc3ec25280cb9f56debde65dacaa89295
Jose AlVarez <Jose07alvarezm@gmail.com>
2023-04-25 22:42:37 -0500

    feat: automatizacion comparacion para varias monedas

--- testeo.py
+++ testeo.py
@@ -8,15 +8,14 @@ import time
 
 
 # Enlace conexión a binance
-def funcion_comparacion(monedas):
+def funcion_comparacion(monedas:list):
     cm_futures_client = CMFutures(key="3a6Q0unsfYV8RXpehFENffnE4hgbWcNAaRRVjrEM99dUTt05l9vPQR1tCeZ8RTOh", secret="aDW3Bwd0POckjdUrBm68kMYdm5CGpBW4FiWzVopLPkGuSOhqlwLLevy2VjOEkwIL")
     client = Client(api_key= "3a6Q0unsfYV8RXpehFENffnE4hgbWcNAaRRVjrEM99dUTt05l9vPQR1tCeZ8RTOh", api_secret="aDW3Bwd0POckjdUrBm68kMYdm5CGpBW4FiWzVopLPkGuSOhqlwLLevy2VjOEkwIL")
 
     Dataframes_monedas = {}
 
     for moneda in monedas:
-        candles = client.get_klines(symbol= moneda, interval=Client.KLINE_INTERVAL_1HOUR, limit = 21)
-
+        candles = client.get_klines(symbol= moneda, interval=Client.KLINE_INTERVAL_1MINUTE, limit = 1000)
         price_df = pd.DataFrame(candles, columns=['dateTime',
                                                 'open',
                                                 'high', 'low',
@@ -36,23 +35,22 @@ def funcion_comparacion(monedas):
 
         ##-------------------------------------------------------- extraemos lista con precios close-------------------------
         lista_precios = [ element for element in price_df['close']]
-        Dataframes_monedas[str(moneda)+ '_df'] = lista_precios
 
+        #Dataframes_monedas[str(moneda) + '_df'] = lista_precios
 
-    final1 = calcular_bollinger_bands(precios = Dataframes_monedas['ETHUSDT_df'], dias = 20, k = 1)
-    final2 = calcular_bollinger_bands(precios = Dataframes_monedas['BTCUSDT_df'], dias = 20, k = 4)
-    final3 = calcular_bollinger_bands(precios = Dataframes_monedas['XLMUSDT_df'], dias = 20, k = 4)
+        final1 = calcular_bollinger_bands(precios = lista_precios, dias = 1000, k = 1)
 
-    BS = final1["Banda Superior"][20]
-    BI = final1["Banda Inferior"][20]
-    UP = Dataframes_monedas['ETHUSDT_df'][20]
+        dato = 999
+        BS = final1["Banda Superior"][dato] #Banda superior
+        BI = final1["Banda Inferior"][dato] #Banda inferior
+        UP = lista_precios[dato] #Ultimo Precio
 
-    if UP > BS:
-        print("Esta por encima de la banda de bollinger")
-    elif UP < BI:
-        print("Esta por debajo de la banda de bollinger")
-    else:
-        print("Dentro de bandas")
+        if UP > BS:
+            print(f"Moneda:{moneda} --> Esta por encima de la banda de bollinger")
+        elif UP < BI:
+            print(f"Moneda:{moneda} --> Esta por debajo de la banda de bollinger")
+        else:
+            pass
 
 
     """
@@ -77,22 +75,12 @@ tiempo_inicial = time.time()
 
 # Duración del ciclo en segundos
 duracion_ciclo = 15
-n = 0
 
 while True:
-    # Obtener el tiempo actual
-    tiempo_actual = time.time()
-
-    # Calcular la diferencia de tiempo en segundos
-    diferencia_tiempo = tiempo_actual - tiempo_inicial
     # Si ha pasado la duración del ciclo, salir del ciclo
-    if diferencia_tiempo >= duracion_ciclo:
-        funcion_comparacion(['BTCUSDT', 'XLMUSDT', 'ETHUSDT'])
-        print("Comparando nuevamente")
-        tiempo_inicial = time.time()
+    funcion_comparacion(['BTCUSDT', 'XLMUSDT', 'ETHUSDT','EOSUSDT'])
+    print("Comparando nuevamente")
+    tiempo_inicial = time.time()
 
-    # Si no ha pasado la duración del ciclo, hacer algo aquí
-    print(f"van {n} segundos")
-    n += 1
     # Pausar la ejecución del programa durante un segundo
-    time.sleep(1)
+    time.sleep(15) #TODO cambiar a 60 s cuando ya vaya a dejarse funcionando
